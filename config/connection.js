3187205ca8abeaa6eaeaa4233c177abd59a0f753
John Harper <jharper1@tulane.edu>
2019-11-23 19:14:29 -0500

    JAWS DB connection fix

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 var mysql = require('mysql');
 var connection;
 
-if (process.send.JAWSDB_URL) {
-    connection = mysql.createConnection(process.send.JAWSDB_URL)
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
 
 var connection = mysql.createConnection({
